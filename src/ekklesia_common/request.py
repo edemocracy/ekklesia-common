58291797cd432f724e4ad222eb4c5698bd6e031e
Tobias dpausp <dpausp@posteo.de>
2021-08-17 14:12:24 +0000

    EkklesiaRequest.render_template: better render error reporting
    Throw an RenderTemplateError when rendering the Jinja template fails
    and wrap the actual render call with an eliot action to log the original
    error directly.

--- src/ekklesia_common/request.py
+++ src/ekklesia_common/request.py
@@ -1,12 +1,18 @@
 from functools import cached_property
 from typing import Any
 import morepath
+from eliot import start_action
 from ekklesia_common import database
 from ekklesia_common.permission import Permission
 from jinja2 import Template
 from sqlalchemy.orm import Session, Query
 
-
+class RenderTemplateError(Exception):
+    def __init__(self, template_name, cause) -> None:
+        self.template_name = template_name
+        cause_type = type(cause).__name__
+        msg = f"Template {template_name} could not be rendered because of an exception: {cause_type}: {cause}"
+        super().__init__(msg)
 
 class EkklesiaRequest(morepath.Request):
 
@@ -31,9 +37,15 @@ class EkklesiaRequest(morepath.Request):
     def q(self, *args, **kwargs) -> Query:
         return self.db_session.query(*args, **kwargs)
 
-    def render_template(self, template: str, **context) -> str:
-        jinja_template: Template = self.app.jinja_env.get_template(template)
-        return jinja_template.render(**context)
+    def render_template(self, name: str, **context) -> str:
+        with start_action(action_type="template-get", name=name):
+            jinja_template: Template = self.app.jinja_env.get_template(name)
+        try:
+            with start_action(action_type="template-render", filename=jinja_template.filename):
+                return jinja_template.render(**context)
+
+        except Exception as e:
+            raise RenderTemplateError(name, e) from e
 
     def flash(self, message, category="primary"):
         flashed_messages = self.browser_session.setdefault("flashed_messages", [])
